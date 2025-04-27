8cc09604f85d15f102f85ec790234cf8329999ca
rears <mariselvan@rears.in>
2025-04-28 00:05:01 +0530

    project image updated

--- src/components/ProjectsIcons/index.tsx
+++ src/components/ProjectsIcons/index.tsx
@@ -16,7 +16,7 @@ export default function ProjectsIcons() {
           <div className="flex flex-row items-center justify-evenly gap-8 md:gap-43">
             <div className="w-1/3 flex items-center justify-center">
               <Image
-                src={"/image/Pluginlive.svg"}
+                src={"/image/eswari.png"}
                 alt="Pluginlive Image"
                 width={150}
                 height={150}
@@ -26,41 +26,8 @@ export default function ProjectsIcons() {
             </div>
             <div className="w-1/3 flex items-center justify-center">
               <Image
-                src={"/image/TalAiro.svg"}
+                src={"/image/arignar.png"}
                 alt="TalArio Image"
-                width={150}
-                height={150}
-                className="w-full h-full object-contain"
-                priority
-              />
-            </div>
-            <div className="w-1/3 flex items-center justify-center">
-              <Image
-                src={"/image/Hase.svg"}
-                alt="Hase Image"
-                width={150}
-                height={150}
-                className="w-full h-full object-contain"
-                priority
-              />
-            </div>
-          </div>
-          <div className="flex flex-row items-center justify-evenly gap-8 md:gap-43">
-            <div className="w-1/3 flex items-center justify-center">
-              <div className="w-[120px] h-[120px] md:w-[150px] md:h-[150px] lg:w-[180px] lg:h-[180px] relative">
-                <Image
-                  src="/image/DucoLogo 1.svg"
-                  alt="DucoLogo Image"
-                  fill
-                  className="object-contain"
-                  priority
-                />
-              </div>
-            </div>
-            <div className="w-1/3 flex items-center justify-center">
-              <Image
-                src={"/image/Blujin.svg"}
-                alt="Blujin Image"
                 width={150}
                 height={150}
                 className="w-full h-full object-contain"
@@ -86,6 +53,48 @@ export default function ProjectsIcons() {
                 </div>
               </div>
             </div>
+          </div>
+          <div className="flex flex-row items-center justify-evenly gap-8 md:gap-43">
+            {/* <div className="w-1/3 flex items-center justify-center">
+              <div className="w-[120px] h-[120px] md:w-[150px] md:h-[150px] lg:w-[180px] lg:h-[180px] relative">
+                <Image
+                  src="/image/DucoLogo 1.svg"
+                  alt="DucoLogo Image"
+                  fill
+                  className="object-contain"
+                  priority
+                />
+              </div>
+            </div>
+            <div className="w-1/3 flex items-center justify-center">
+              <Image
+                src={"/image/Blujin.svg"}
+                alt="Blujin Image"
+                width={150}
+                height={150}
+                className="w-full h-full object-contain"
+                priority
+              />
+            </div> */}
+            {/* <div className="w-1/3 flex items-center justify-center">
+              <div className="flex items-center gap-2">
+                <div>
+                  <Image
+                    src={"/image/Skymute.svg"}
+                    alt="Skymute Image"
+                    width={98}
+                    height={93}
+                    className="w-full h-full object-contain"
+                    priority
+                  />
+                </div>
+                <div>
+                  <h1 className="text-base md:text-2xl lg:text-4xl font-sans font-medium custom-color-p">
+                    Skymute
+                  </h1>
+                </div>
+              </div>
+            </div> */}
           </div>
         </div>
       </div>
